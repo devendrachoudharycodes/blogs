f1eab43ca1870aca25109fa47e45af91cb6c2d5a
devendrachoudharycodes <devendra.choudhary.codes@gmail.com>
2026-09-10 00:07:12 +0530

    fix:added url mapping to HeaderLink.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://devendrachoudharycodes.github.io',
-	base: '/blogs',
+	base: '/blogs/',
 
 	integrations: [mdx(), sitemap()],
 
