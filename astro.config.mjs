46558686d67041cf603205c17c35bfdf3590f6b5
devendrachoudharycodes <devendra.choudhary.codes@gmail.com>
2026-09-09 23:55:58 +0530

    changed astro config to make compatible with github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,11 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://devendrachoudharycodes.github.io',
+	base: '/blogs',
+
 	integrations: [mdx(), sitemap()],
+
 	fonts: [
 		{
 			provider: fontProviders.local(),
